698e3114f3ce660a2bbb4cb588006635fba1afa3
RDCLder <riy2@case.edu>
2019-01-20 16:01:45 -0600

    updated -ray

---
+++ models/database.js
@@ -0,0 +1,17 @@
+let promise = require('bluebird');
+
+const initOptions = {
+    promiseLib: promise
+}
+
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'fullstackDB',
+    user: 'rdclder'
+}
+
+const pgp = require('pg-promise')(initOptions);
+const db = pgp(config);
+
+module.exports = db;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models/database.js");
+
+router.get("/", (req, res) => {
+    res.render("index", {
+        pageTitle: "Home",
+        pageID: "home"
+    });
+});
+
+module.exports = router;
