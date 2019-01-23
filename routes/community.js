2b9fd8e9d6b2ff8c1ad223fe2ebaacbaa3ac1ad8
Phong Mach <pqmach77@gmail.com>
2019-01-23 14:35:30 -0600

    reformating community page

--- routes/community.js
+++ routes/community.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models/database.js");
+// const db = require("../models/database.js");
 
 router.get('/community', (req,res) => {
     res.render("community", {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models/database.js");
+// const db = require("../models/database.js");
 
 router.get("/", (req, res) => {
     res.render("index", {
