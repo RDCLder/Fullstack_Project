b0e869ab63310e990526c8dc4b14b257f0c4d22d
RDCLder <riy2@case.edu>
2019-01-24 12:51:28 -0600

    test -ray

--- routes/login.js
+++ routes/login.js
@@ -1,73 +1,51 @@
-// const express = require("express");
-// const router = express.Router();
-// const db = require("../models/index.js");
-
-// router.get("/login", passport.authenticate("local", {
-//     successRedirect: "/",
-//     failureRedirect: "/login",
-//     failureFlash: true 
-// }));
-
-module.exports = router;
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('./../models');
-const LocalStrategy = require('passport-local').Strategy
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
-const cookieParser = require('cookie-Parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const db = require("./../models");
+const LocalStrategy = require("passport-local").Strategy
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-Parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+//const Sequelize = require("sequelize");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Middleware
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
+router.use(cookieParser());
 
 var myStore = new SequelizeStore({
     db: db.sequelize
-})
+});
 router.use(session({
-    secret: 'mySecret',
+    secret: "keyboard cat",
     store: myStore,
     resave: false,
     proxy: true
-}))
+}));
 myStore.sync();
 
-router.use(cookieParser());
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.get('/login', function (req, res) {
-    res.send(
-        ` 
-            <h1>Login</h1>
-            <form action="/login" method="post">
-                <div>
-                    <label>Username:</label>
-                    <input type="text" name="username"/>
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" name="password"/>
-                </div>
-                <div>
-                    <input type="submit" value="Log In"/>
-                </div>
-            </form>
-        `
-    ); //end of res.send
-});//end of app.get
+router.get("/login", (req, res) => {
+    res.render("/login", {
+        pageTitle: "Login",
+        pageID: "login"
+    });
+});
 
-router.post(
-    "/login",
+router.post("/login",
     passport.authenticate("local", {
-        successRedirect: "/dashboard",
-        failureRedirect: "/login"
+        successRedirect: "back",
+        failureRedirect: "/login",
+        failureFlash: true
     })
 );
 
 passport.use(new LocalStrategy((username, password, done) => {
+    console.log("Im in passp;ort");
     db.users.findAll({ where: { username: username } }).then((results) => {
 
         if (results != null) {
@@ -88,11 +66,11 @@ passport.use(new LocalStrategy((username, password, done) => {
             done(null, false)
         }
     })
-}))
+}));
 
 passport.serializeUser((user, done) => {
     done(null, user.id)
-})
+});
 
 passport.deserializeUser((id, done) => {
     db.users.findById(parseInt(id, 10)).then((data) => {
