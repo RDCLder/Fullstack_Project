184f717ee94883acc7ad7301dcd2ccc4be78f733
RDCLder <riy2@case.edu>
2019-01-25 15:31:14 -0600

    updated

--- routes/login.js
+++ routes/login.js
@@ -29,12 +29,12 @@ myStore.sync();
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.get('/login', function (req, res) {
-    res.render('login',{
-        pageTitle: "Login",
-        pageId: "login"
-    }); //end of res.send
-});//end of app.get
+// router.get('/login', function (req, res) {
+//     res.render('login',{
+//         pageTitle: "Login",
+//         pageId: "login"
+//     }); //end of res.send
+// });//end of app.get
 
 router.post("/login",
     passport.authenticate("local", {
