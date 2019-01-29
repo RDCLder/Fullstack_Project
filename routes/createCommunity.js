1c09535817ca902b5eb888984a24f66767445768
Phong Mach <pqmach77@gmail.com>
2019-01-28 21:12:05 -0600

    finished  discussTopic ejs, working on createCommunity ejs

--- routes/createCommunity.js
+++ routes/createCommunity.js
@@ -2,4 +2,11 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/");
 
+router.get('/createCommunity',(req, res)=>{
+    res.render('createCommunity',{
+        pageTitle: "Create Community",
+        isLoggedIn: false
+    })
+})
+
 module.exports = router;
