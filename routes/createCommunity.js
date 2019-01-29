0b09e70853e1f28d98a376e4ae05db1d427ae97a
Phong Mach <pqmach77@gmail.com>
2019-01-29 12:44:30 -0600

    got the create community to work

--- routes/createCommunity.js
+++ routes/createCommunity.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models/");
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/createCommunity',(req, res)=>{
-    res.render('createCommunity',{
-        pageTitle: "Create a Community",
-        isLoggedIn: false,
-        pageType: "createCommunity"
+    console.log(req.user)
+    if (!req.user) {
+        console.log("NOT AUTHENTICATED IN CREATE COMMUNITY!");
+        res.redirect("/login");
+    }
+    else{
+        console.log("AUTHENTICATED IN CREATE COMMUNITY!");
+        res.render('createCommunity',{
+            pageTitle: "Create a Community",
+            pageID: "createCommunity",
+            isLoggedIn: false,
+            pageType: "createCommunity"
+        })
+    }
+})
+
+router.post('/createCommunity',(req,res)=>{
+    var name = req.body.name;
+    var description = req.body.description;
+    db.community.create({
+        name: name,
+        description: description
     })
+    res.redirect('..')
 })
 
 module.exports = router;
