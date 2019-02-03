937fcb94360cd571dc4ce083685248ea19f6735a
Phong Mach <pqmach77@gmail.com>
2019-02-03 13:40:01 -0600

    changes

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((req, res) => {
 
 // db.sequelize.migrate;
 // db.sequelize.drop();
-// db.sequelize.sync();
+db.sequelize.sync();
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
