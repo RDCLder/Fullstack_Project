81c60d371b7c76db05390fefc807cf7c018e7afa
Phong Mach <pqmach77@gmail.com>
2019-02-05 14:58:46 -0600

    made changes to the port

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use((req, res) => {
 // app.listen(3000, ()=>{
 //     console.log('listening on port 3000')
 // })
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
     console.log("App is running on port " + port);
 });
