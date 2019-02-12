53ea3a1a99903f76b597e2ff03a11f89b25ff24b
RDCLder <riy2@case.edu>
2019-02-11 20:24:00 -0600

    modularized current time calc into timeAgo function -ray

--- routes/topic.js
+++ routes/topic.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/");
 const body_parser = require("body-parser");
+const timeAgo = require("../functions/timeAgo");
 
 router.get("/topic", (req, res) => {
     res.redirect("/");
@@ -42,66 +43,14 @@ router.get("/topic/:topicID", (req, res) => {
             if (comment.length > 0) {
 
                 let timestamp = comment[0].dataValues.topic.dataValues.createdAt;
-                let currentTime = new Date();
-                let timeDifference = Math.abs(currentTime.getTime() - timestamp.getTime());
-
-                if (timeDifference < 60000) {
-                    var ago = "just now";
-                }
-                else if (60000 <= timeDifference && timeDifference < 3600000) {
-                    let diffMinutes = Math.abs(Math.floor(timeDifference / (1000 * 60)));
-                    var ago = `${diffMinutes} minutes ago`;
-                }
-                else if (3600000 <= timeDifference && timeDifference < 86400000) {
-                    let diffHours = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60)));
-                    var ago = `${diffHours} hours ago`;
-                }
-                else if (86400000 <= timeDifference && timeDifference < 2592000000) {
-                    let diffDays = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60 * 24)));
-                    var ago = `${diffDays} days ago`;
-                }
-                else if (2592000000 <= timeDifference && timeDifference < 77760000000) {
-                    let diffMonths = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30)));
-                    var ago = `${diffMonths} months ago`;
-                }
-                else if (77760000000 <= timeDifference) {
-                    let diffYears = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30 * 12)));
-                    var ago = `${diffYears} years ago`;
-                }
-                let topicTime = ago;
+                let topicTime = timeAgo(timestamp);
 
                 let commentTimes = [];
                 for (let i = 0; i < comment.length; i++) {
-                    
                     let timestamp2 = comment[i].dataValues.createdAt;
-                    let timeDifference2 = Math.abs(currentTime.getTime() - timestamp2.getTime());
-                    
-                    if (timeDifference2 < 60000) {
-                        var ago2 = "just now";
-                    }
-                    else if (60000 <= timeDifference2 && timeDifference2 < 3600000) {
-                        let diffMinutes2 = Math.abs(Math.floor(timeDifference2 / (1000 * 60)));
-                        var ago2 = `${diffMinutes2} minutes ago`;
-                    }
-                    else if (3600000 <= timeDifference2 && timeDifference2 < 86400000) {
-                        let diffHours2 = Math.abs(Math.floor(timeDifference2 / (1000 * 60 * 60)));
-                        var ago2 = `${diffHours2} hours ago`;
-                    }
-                    else if (86400000 <= timeDifference2 && timeDifference2 < 2592000000) {
-                        let diffDays2 = Math.abs(Math.floor(timeDifference2 / (1000 * 60 * 60 * 24)));
-                        var ago2 = `${diffDays2} days ago`;
-                    }
-                    else if (2592000000 <= timeDifference2 && timeDifference2 < 77760000000) {
-                        let diffMonths2 = Math.abs(Math.floor(timeDifference2 / (1000 * 60 * 60 * 24 * 30)));
-                        var ago2 = `${diffMonths2} months ago`;
-                    }
-                    else if (77760000000 <= timeDifference2) {
-                        let diffYears2 = Math.abs(Math.floor(timeDifference2 / (1000 * 60 * 60 * 24 * 30 * 12)));
-                        var ago2 = `${diffYears2} years ago`;
-                    }
-                    commentTimes.push(ago2);
+                    let commentTime = timeAgo(timestamp2);
+                    commentTimes.push(commentTime);
                 }
-                console.log(commentTimes);
                 
                 if (!req.user) {
                     res.render("topic", {
@@ -150,32 +99,7 @@ router.get("/topic/:topicID", (req, res) => {
                     .then(topic => {
 
                         let timestamp = topic[0].dataValues.createdAt;
-                        let currentTime = new Date();
-                        let timeDifference = Math.abs(currentTime.getTime() - timestamp.getTime());
-
-                        if (timeDifference < 60000) {
-                            var ago = "just now";
-                        }
-                        else if (60000 <= timeDifference && timeDifference < 3600000) {
-                            let diffMinutes = Math.abs(Math.floor(timeDifference / (1000 * 60)));
-                            var ago = `${diffMinutes} minutes ago`;
-                        }
-                        else if (3600000 <= timeDifference && timeDifference < 86400000) {
-                            let diffHours = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60)));
-                            var ago = `${diffHours} hours ago`;
-                        }
-                        else if (86400000 <= timeDifference && timeDifference < 2592000000) {
-                            let diffDays = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60 * 24)));
-                            var ago = `${diffDays} days ago`;
-                        }
-                        else if (2592000000 <= timeDifference && timeDifference < 77760000000) {
-                            let diffMonths = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30)));
-                            var ago = `${diffMonths} months ago`;
-                        }
-                        else if (77760000000 <= timeDifference) {
-                            let diffYears = Math.abs(Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30 * 12)));
-                            var ago = `${diffYears} years ago`;
-                        }
+                        let topicTime = timeAgo(timestamp);
 
                         if (!req.user) {
                             res.render("topic", {
@@ -184,7 +108,7 @@ router.get("/topic/:topicID", (req, res) => {
                                 pageType: "topic",
                                 comments: [],
                                 topic: topic[0],
-                                topicTime: ago,
+                                topicTime: topicTime,
                                 community: topic[0].dataValues.community.dataValues,
                                 isLoggedIn: false
                             });
@@ -195,7 +119,7 @@ router.get("/topic/:topicID", (req, res) => {
                                 pageType: "topic",
                                 comments: [],
                                 topic: topic[0],
-                                topicTime: ago,
+                                topicTime: topicTime,
                                 community: topic[0].dataValues.community.dataValues,
                                 isLoggedIn: true,
                                 user: req.user
